eb9c3dd82d140fd9934af0861a83de0a69654a06
Mohammed Ameen <jegroamin@gmail.com>
2018-08-19 13:19:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function BoardMember(name, homeState, training){
     return "No, I must disagree";
   }
   this.approve = function(){
-    return "No, I must disagree";
+    return "You can do that!";
   }
   this.veto = function(){
     return "No, I must disagree";
